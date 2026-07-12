c49369985e654f45c8cc5280d70c589736a40206
tara210 <ramasamytara@gmail.com>
2026-07-12 14:47:03 +0200

    Final setup with image resolution
    fixed image resolution

--- 01_experiment/stimuli/creatingDataStructure/tts_pipeline.py
+++ 01_experiment/stimuli/creatingDataStructure/tts_pipeline.py
@@ -6,7 +6,8 @@ import numpy as np
 import csv
 import librosa
 import pyrubberband as pyrb
-
+import subprocess
+import os
 
 pipeline = KPipeline(lang_code='a')  
 
@@ -18,7 +19,7 @@ with open ("sentences_fromGdoc.csv",mode='r') as f:
         print(a)
         #perform tts for both restrictive and non restrictive version. 
         for indx in (1,2):
-            generator = pipeline(a[1], voice='af_heart',speed=0.65)  
+            generator = pipeline(a[indx], voice='af_heart',speed=0.65)  
             
             # Collect all audio chunks
             audio_chunks = []
@@ -32,33 +33,33 @@ with open ("sentences_fromGdoc.csv",mode='r') as f:
             #full_audio=librosa.effects.time_stretch(concat_audio, rate=0.65)
             #pitch_down=librosa.effects.pitch_shift(full_audio,n_steps=-3,sr=24000)
 
-
+            tmpOUT="tmp_out.wav"
             id=a[0]
             if indx==1:
                 pathTmp=f"{id}_r.wav"
                 sf.write(pathTmp, full_audio, 24000)
                 print("Saved output.wav")
-                SPEED_RATE = 0.65
-                FADE_DURATION = 0.08  
+                SPEED_RATE = 1
+                TRIM_START = 0.5
                 subprocess.run([
                 "ffmpeg", "-y",
                 "-i", pathTmp,
-                "-filter:a", f"atempo={SPEED_RATE},afade=t=in:st=0:d={FADE_DURATION}",
-                pathTmp
+                "-filter:a", f"atempo={SPEED_RATE},atrim=start={TRIM_START},asetpts=PTS-STARTPTS",
+                tmpOUT
                 ], check=True)
             elif indx==2:
                 pathTmp=f"{id}_n.wav"
                 sf.write(pathTmp, full_audio, 24000)
                 print("Saved output.wav")
-                SPEED_RATE = 0.65
-                FADE_DURATION = 0.08  
+                SPEED_RATE = 1
+                TRIM_START = 0.5
                 subprocess.run([
                 "ffmpeg", "-y",
                 "-i", pathTmp,
-                "-filter:a", f"atempo={SPEED_RATE},afade=t=in:st=0:d={FADE_DURATION}",
-                pathTmp
+                "-filter:a", f"atempo={SPEED_RATE},atrim=start={TRIM_START},asetpts=PTS-STARTPTS",
+                tmpOUT
                 ], check=True)
-
+            os.replace(tmpOUT, pathTmp)
             
 
             #now we need to perform forced alignment on the audio

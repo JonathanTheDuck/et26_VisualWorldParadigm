92d0bc2483dccea27b2c1394def7c30bb3dd78ae
klenkjon <jonathan.klenk@gmail.com>
2026-08-25 21:26:04 +0200

    current state. pretty messy
    :/

--- 02_analytics/human_predictions/human_pipeline_rawGaze.py
+++ 02_analytics/human_predictions/human_pipeline_rawGaze.py
@@ -4,9 +4,11 @@ import pandas as pd
 partID=[]
 out_stimuliId=[]
 out_obj=[]
+is_target=[]
+out_count_per_obj_per_part=[]
 out_percent=[]
 condition=[]
-for participantNumber in range(1,6):
+for participantNumber in range(1,24):
 
     #collect everything in one dataframe for ease of use with llm based percentages and aggregation
     
@@ -14,7 +16,7 @@ for participantNumber in range(1,6):
     subjectnr=participantNumber
     groupNr=subjectnr %8+1
 
-    path="../../01_experiment/recordings/validParticipants/"
+    path="../../01_experiment/recordings/SUBJECTS (FINAL)/"
     path_info="../../01_experiment/stimuli/creatingDataStructure/participant_groups_8/participant_group"+str(groupNr)+".csv"
     part_df_tmp=pd.read_csv(path+"subject-"+str(subjectnr)+".csv")
     gazeSamples_df=pd.read_table(path+"subject-"+str(subjectnr)+".tsv")
@@ -26,14 +28,14 @@ for participantNumber in range(1,6):
     annotationPath="../../01_experiment/stimuli/annotation_audiov2.csv"
     annotation_df=pd.read_csv(annotationPath)
 
-    part_df=part_df_tmp[["id","position1","position2","position3","position4","count_audio_file_offset","audio_file","condition"]]
+    part_df=part_df_tmp[["id","position1","position2","position3","position4","count_audio_file_offset","audio_file","condition","target_position"]]
     part_df.rename(columns={'count_audio_file_offset': 'trialID'}, inplace=True)
     part_df.rename(columns={'id': 'stimuliID'}, inplace=True)
     part_df
 
     #adding trial ids
     is_start = gazeSamples_df_reduced["USER"].eq("START_TRIAL")
-    is_stop = gazeSamples_df_reduced["USER"].eq("STOP_TRIAL")
+    is_stop = gazeSamples_df_reduced["USER"].eq("AUDIO_FILE_OFFSET") #because the stop trial seems to have sometimes not been recorded correctly. 
 
     inside = False
     current_id = -1
@@ -55,24 +57,16 @@ for participantNumber in range(1,6):
     samples_onlyTrials = gazeSamples_df_reduced[gazeSamples_df_reduced["trialId"].notna()]
     samples_onlyTrials
 
-    positions_old = {
-        "pos1": (1782, 378),
-        "pos2": (1342, 825),
-        "pos3": (657, 825),
-        "pos4": (217, 378),
-    }
-    #________________________________________
-    #we have to use scaled positions though as we displayed a 2560x1440 image on 1920x1080 screen.
     positions = {
-        "pos1": (1745.6, 462.4),
-        "pos2": (1393.6, 820.0),
-        "pos3": (845.6,  820.0),
-        "pos4": (493.6,  462.4),
+        "pos1": (1891, 429),
+        "pos2": (1423,915),
+        "pos3": (697, 915),
+        "pos4": (229, 429),
     }
 
     #also scaled these so they are not sqares anymore actually...need to take care of rsolution better next time !
-    box_width = 320
-    box_height = 309
+    BOX_WIDTH = 440
+    BOX_HEIGTH = 440
     #________________________________________
 
     #also it seems the screen resolution was at 1920x1080, if i read it coirrectly fromm the logfile. so we should change this for the real trials!
@@ -93,7 +87,7 @@ for participantNumber in range(1,6):
     SCREEN_W, SCREEN_H = 2560, 1440  # OpenSesame virtual canvas
     IMG_W, IMG_H = 2560, 1440        # from your image
 
-    SCALE = 0.8  # explicitly set in OpenSesame sketchpad
+    SCALE = 1  # explicitly set in OpenSesame sketchpad
 
     disp_w = IMG_W * SCALE    # 1920.0
     disp_h = IMG_H * SCALE    # 1120.0
@@ -122,8 +116,8 @@ for participantNumber in range(1,6):
     for name, (x, y) in positions.items():
         rect = Rectangle(
             (x, y),            # top-left corner
-            box_width,         # width
-            box_height,        # height
+            BOX_WIDTH,         # width
+            BOX_HEIGTH,        # height
             linewidth=3,
             edgecolor='blue',
             facecolor='none'
@@ -136,8 +130,10 @@ for participantNumber in range(1,6):
     ax.set_title(f"{len(valid)} valid gaze samples on screen ({SCREEN_W}x{SCREEN_H})")
     ax.legend(loc="upper right")
     plt.tight_layout()
-    plt.show()
-    #maybe output this as sanity check inbetween steps?
+
+    #(for sanity checking. even better if you check for all actual stimuli img instead of one. boundiing boxes will remain the same though)
+    #plt.show()
+    
 
     onsetsetsAudioFiles=samples_onlyTrials[samples_onlyTrials["USER"]=="AUDIO_FILE_ONSET_LOG"]
     onsetsetsAudioFiles.rename(columns={"TIME":"audio_onset_time"},inplace=True)
@@ -174,8 +170,6 @@ for participantNumber in range(1,6):
         
         audioStart_trial=onsetsetsAudioFiles[onsetsetsAudioFiles["trialId"]==str(trialNr)]["audio_onset_time"].values[0]
 
-        #position mappings:
-
         #start and ending time:
         trial_annotation_df=annotation_df[annotation_df["stimuliID"]==stimuliID]
         trial_annotation_df=trial_annotation_df[trial_annotation_df["condition"]==condition_trial]
@@ -199,46 +193,77 @@ for participantNumber in range(1,6):
         valid_trial = samples_restricted[samples_restricted["BPOGV"] == 1].copy()
         valid_trial["screen_x"] = valid_trial["BPOGX"] * SCREEN_W
         valid_trial["screen_y"] = valid_trial["BPOGY"] * SCREEN_H
-
+        #print(valid_trial.to_string())
         #now we check positioning in one of the 4 aois
         mappedToPositions=assign_aoi(valid_trial,positions,
-                                    box_width = 320,box_height = 300,
+                                    box_width = BOX_WIDTH,box_height = BOX_HEIGTH,
                                     x_col="screen_x",y_col="screen_y")
-        
+        #print(mappedToPositions.to_string())
         #count_inANY_AOI=mappedToPositions.shape[0]
-        count_perAOI_df=mappedToPositions.groupby("AOI").size()
-        percent_perAOI_df=count_perAOI_df/count_perAOI_df.sum()
+        count_perAOI_df = (
+    mappedToPositions.groupby("AOI")
+    .size()
+    .reset_index(name="count")
+)
+        #print(count_perAOI_df.to_string()) if not count_perAOI_df.empty else print("no gaze in any AOI")
+        #print(count_perAOI_df)
+        #percent_perAOI_df=count_perAOI_df/count_perAOI_df.sum()
+        #print(percent_perAOI_df)
+        
+        #percentages_trial = (
+        #mappedToPositions.groupby("AOI")
+        #.size()
+        #.reindex(["pos1", "pos2", "pos3", "pos4"], fill_value=0)
+       # )
 
-        percentages_trial = (
-        mappedToPositions.groupby("AOI")
-        .size()
-        .reindex(["pos1", "pos2", "pos3", "pos4"], fill_value=0)
-        )
-
-        percentages_trial = percentages_trial / percentages_trial.sum()
-
+        #percentages_trial = percentages_trial / percentages_trial.sum()
+        
 
         #before saving the percentages with objects we should also get the object strings so we can aggregate on that 
-
+        t_pos=trial_info["target_position"].values[0]
         for  i,pos in enumerate(["pos1","pos2","pos3","pos4"]):
-            print(i)
+            #print(i)
             #in the further pipeline we use stimuli ids starting at 1 !!
             out_stimuliId.append(stimuliID+1)
             out_obj.append(trial_info["position"+str(i+1)].values[0])
-            outP = 0.0 if pd.isna(percentages_trial[pos]) else percentages_trial[pos]
-            out_percent.append(outP)
+            
+            if i+1==t_pos:
+                is_target.append(1)
+            else:
+                is_target.append(0)
+            #outP = 0.0 if pd.isna(percentages_trial[pos]) else percentages_trial[pos]
+            out_count_per_obj_per_part.append(count_perAOI_df[count_perAOI_df["AOI"]==pos]["count"].values[0] if not count_perAOI_df[count_perAOI_df["AOI"]==pos].empty else 0)
+            
+            #out_count_per_obj_per_part.append(outCount)
             partID.append(participantNumber)
             condition.append(condition_trial)
 
 
-out_df = pd.DataFrame({
+out_df_source = pd.DataFrame({
     "stimuliId": out_stimuliId,
     "partID": partID,
     "condition":condition,
     "obj": out_obj,
-    "percent": out_percent
+    "is_target":is_target,
+    "count_per_obj_per_part": out_count_per_obj_per_part
     
 })                               
 
-out_df.to_csv("output_all.csv", index=False)
-trial_info
+
+
+#count non zero participoants ie poarticipants that contributed to the overall sum on any of the objects
+count_intel_df= out_df_source.groupby(["stimuliId","condition","partID"]).agg({"count_per_obj_per_part":"sum"}).reset_index()
+count_nonZeroParts_df=count_intel_df[count_intel_df["count_per_obj_per_part"] > 0].groupby(["stimuliId","condition"]).size().reset_index(name="countOfNonZeroParticipants")
+count_nonZeroParts_df["countOfNonZeroParticipants"] = (
+    count_nonZeroParts_df["countOfNonZeroParticipants"].fillna(0)
+)
+print(count_nonZeroParts_df.to_string())
+
+out_df=out_df_source.groupby(["stimuliId","condition","obj"]).agg({"count_per_obj_per_part":"sum"}).reset_index()
+out_df["percentages"]=out_df["count_per_obj_per_part"]/out_df.groupby(["stimuliId","condition"])["count_per_obj_per_part"].transform("sum")
+out_df["count_per_obj_per_part"] = (
+    out_df["count_per_obj_per_part"].fillna(0)
+)
+out_df = out_df.merge(count_nonZeroParts_df, on=["stimuliId","condition"], how="left")
+out_df = out_df.merge(out_df_source[["stimuliId","condition","obj","is_target"]],on=["stimuliId","condition","obj"], how="left")
+out_df.to_csv("output_all_withPercentages.csv", index=False)
